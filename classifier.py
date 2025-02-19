e9211beb52fc1a517114456618ea128bd9fc884a
hilmibukhori5 <hilmibukhori5@gmail.com>
2025-02-19 16:48:30 +0700

    Add comment

--- classifier.py
+++ classifier.py
@@ -3,7 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-
+# sndjasajfnjakdfnjdfdkfnadf jhsadbjdsfbdsfbhsdjbh
 # Load dataset
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
@@ -20,4 +20,3 @@ y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
 print(f"Model Accuracy: {accuracy:.2f}")
-print("Classification complete!!!!!!!!!!!")
